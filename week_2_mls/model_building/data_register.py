c618b1d3d24d541d4cf63d59fcd4288c03cbd9e2
drkaruna <drkaruna.batra@gmail.com>
2026-07-07 11:17:08 +0000

    Add ML pipeline project

--- week_2_mls/model_building/data_register.py
+++ week_2_mls/model_building/data_register.py
@@ -1,19 +1,21 @@
-from huggingface_hub.utils import RepositoryNotFoundError
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
 
 repo_id = "Verallon/Machine-Failure-Prediction"
 repo_type = "dataset"
 
+# Initialize API client
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
+# Step 1: Check if the space exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Dataset repo '{repo_id}' already exists. Using it.")
+    print(f"Space '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
-    print(f"Dataset repo '{repo_id}' not found. Creating new one...")
+    print(f"Space '{repo_id}' not found. Creating new space...")
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Dataset repo '{repo_id}' created successfully.")
+    print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
     folder_path="week_2_mls/data",
